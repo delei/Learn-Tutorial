36334d23d4f0cc5413adb503d420522ffdb59def
deleiguo <deleiguo@datadriver.com.cn>
2021-03-02 23:59:53 +0800

    update LinkedListDemo

--- source/java/interview/src/main/java/cn/delei/java/basic/LinkedListDemo.java
+++ source/java/interview/src/main/java/cn/delei/java/basic/LinkedListDemo.java
@@ -13,7 +13,8 @@ public class LinkedListDemo {
     public static LinkedList<Integer> dataList = new LinkedList<>();
 
     public static void main(String[] args) {
-        add();
+//        add();
+        opera();
     }
 
 
@@ -39,6 +40,30 @@ public class LinkedListDemo {
         }
         stopWatch.stop();
         System.err.println(stopWatch.prettyPrint());
+    }
+
+    static void opera() {
+        int size = 10;
+        dataList = new LinkedList<>();
+        dataList.add(null);
+        for (int i = 0; i < size; i++) {
+            dataList.add(i);
+        }
+        for (int i = 0; i < size; i++) {
+            dataList.add(i);
+        }
+        System.out.println(dataList);
+        System.out.println(dataList.peek());
+        System.out.println(dataList.getFirst());
+        System.out.println(dataList.indexOf(5));
+        System.out.println(dataList.lastIndexOf(5));
+        dataList.remove();
+        System.out.println(dataList);
+        // 只会移除第一个5
+        dataList.remove(Integer.valueOf(5));
+        System.out.println(dataList);
+        dataList.remove(2);
+        System.out.println(dataList);
     }
 
 }
