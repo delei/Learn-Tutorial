7522b5c3cf2e1f6e85c37f69e3fe7ae983797b9c
DeleiGuo <deleiguo@datadriver.com.cn>
2021-03-18 18:13:57 +0800

    add Syntactic Sugar Demo

---
+++ source/java/interview/src/main/java/cn/delei/java/lang/SyntacticSugarDemo.java
@@ -0,0 +1,46 @@
+package cn.delei.java.lang;
+
+public class SyntacticSugarDemo {
+
+    public static void main(String[] args) {
+        switchString();
+        autoBoxing();
+        assertDemo();
+    }
+
+    /**
+     * switch 对 String 支持
+     */
+    static void switchString() {
+        String a = "1";
+        switch (a) {
+            case "1":
+                System.out.println(a);
+                break;
+            case "2":
+                System.out.println("2");
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * 自动拆箱装箱
+     */
+    static void autoBoxing() {
+        Integer i1 = 10;
+        int i2 = 11;
+        Integer i3 = i2;
+    }
+
+    /**
+     * 断言
+     */
+    static void assertDemo() {
+        int a = 1;
+        int b = 1;
+        assert a == b;
+        assert a != b : "Hollis";
+    }
+}
