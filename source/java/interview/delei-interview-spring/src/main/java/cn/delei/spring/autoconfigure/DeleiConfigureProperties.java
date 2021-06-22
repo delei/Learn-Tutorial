cf747584dfd1cc4565f078392552d379693f61f9
deleiguo <deleiguo@datadriver.com.cn>
2021-06-22 23:16:48 +0800

    update config properties

---
+++ source/java/interview/delei-interview-spring/src/main/java/cn/delei/spring/autoconfigure/DeleiConfigureProperties.java
@@ -0,0 +1,50 @@
+package cn.delei.spring.autoconfigure;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * 自动配置文件
+ * @author deleiguo
+ */
+@Component
+@ConfigurationProperties(prefix = "delei.model")
+public class DeleiConfigureProperties {
+
+    private String name;
+    private String age;
+    private String city;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "DeleiConfigureProperties{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
